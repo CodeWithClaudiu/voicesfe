94127d2e49cc4d424e0b1980983469d2ac195305
CodeWithClaudiu <claudiochiticaru77@gmail.com>
2026-03-10 13:04:07 +0100

    FormInsertPost funzionante

--- src/app/form-insert-post/form-insert-post.ts
+++ src/app/form-insert-post/form-insert-post.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BlogPost, Blog,Blogger } from '../model/entities';
+import { BlogPostService } from '../blog-post-service';
 
 @Component({
   selector: 'app-form-insert-post',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-insert-post.html',
   styleUrl: './form-insert-post.css',
 })
 export class FormInsertPost {
 
+  blogPostService = inject(BlogPostService);
+
+  post: BlogPost = {
+    id: 0,
+    title: '',
+    tags: '',
+    content: '',
+    date: '',
+    blog: {
+      id: 0,
+      title: '',
+      description: '',
+      blogger: {
+        id: 0,
+        nickname: '',
+        bio: '',
+      }
+    }
+  };
+
+  errorMessage = signal<string>('');
+  successMessage = signal<string>('');
+
+  normalizeTags(): void{
+    //divide i tag, rimuove spazi, uniforma le maiuscole e rimuove i duplicati
+    let uniqueTags = 
+    new Set(
+    this.post.tags
+      .split(',')
+      .map(tag => tag.trim().toLowerCase())
+      .filter(tag => tag.length > 0)
+    );
+    //ricostruisce la stringa dei tag senza duplicati
+    this.post.tags = Array.from(uniqueTags).join(', ');
+  }
+  
+  //validazione dei campi e invio del post al service
+  savePost(): void {
+    this.errorMessage.set('');
+    this.successMessage.set('');
+    this.normalizeTags();
+
+    if(!this.post.title.trim() || !this.post.content.trim() ||!this.post.tags.trim()){
+      this.errorMessage.set('Titolo, contenuto e tag sono obbligatori.');
+      return;
+    }
+
+    this.post.date = new Date().toISOString();
+    console.log('Post pronto da inviare al service:', this.post);
+
+
+    this.successMessage.set('Post salvato con successo!');
+  }
 }
