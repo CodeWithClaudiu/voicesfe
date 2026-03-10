d5d9bcba5c090d7ccdcbab575d2d4a9230c41435
CodeWithClaudiu <claudiochiticaru77@gmail.com>
2026-03-10 11:45:35 +0100

    Creazione componente FormInsertPost

---
+++ src/app/form-insert-post/form-insert-post.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-insert-post',
+  imports: [],
+  templateUrl: './form-insert-post.html',
+  styleUrl: './form-insert-post.css',
+})
+export class FormInsertPost {
+
+}
